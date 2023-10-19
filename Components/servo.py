5a86fede6c672c3e6f4de3770cd162eb95cdc7b4
LaurenswvMinnen <laurens.v.minnen@gmail.com>
2023-10-19 12:27:15 +0200

    comments

--- Components/servo.py
+++ Components/servo.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
 
+#ik heb de 16 pin op de gpio gebruikt. als er een andere pin gebruikt moet worden, of er zo'n grove aansluiting is voor servo, moeten we de pin veranderen.
 GPIO.setup(16, GPIO.OUT)
 pwm=GPIO.PWM(16, 50)
 
@@ -10,11 +11,12 @@ pwm.start(0)
 def SetAngle(angle):
 
 	duty = angle / 18 + 2
-
+	
 	GPIO.output(16, True)
 
 	pwm.ChangeDutyCycle(duty)
-
+    #deze sleep en output(false) werkt tegen jitter. nadeel is dat dat er maar 1 positie per seconde kan gegeven. ik vermoed dat dit voldoende is en het roer niet 
+    # zo vaak van positie hoeft te veranderen
 	sleep(1)
 
 	GPIO.output(16, False)
@@ -33,3 +35,5 @@ except KeyboardInterrupt:
         GPIO.cleanup()
 
 pwm.stop()
+
+
