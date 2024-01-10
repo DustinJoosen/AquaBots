092d3be1e4984327ab76d0c4b098ad2b05874c97
Dustin Joosen <dustinjoosen2003@gmail.com>
2024-01-10 12:17:53 +0100

    Make main.js more DRY

--- src/GUI/static/main.js
+++ src/GUI/static/main.js
@@ -11,63 +11,24 @@ let send_api_request = (endpoint) => {
         })
 }
 
-document.getElementById("toggle_humidity_btn").addEventListener("change", (event) => {
-    let humidity_on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("humidity_interval").value);
-    let pin = document.getElementById("humidity_pin").value;
+let addToggleEventListener = function (id) {
+    document.getElementById("toggle_${id}_btn").addEventListener("change", (event) => {
+        let on_off = event.target.checked;
+        let interval = parseInt(document.getElementById("${id}_interval").value);
+        let pin = document.getElementById("${id}_pin").value;
 
-    // Send request
-    send_api_request("humidity/" + (humidity_on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
+        // Send request
+        send_api_request(id + "/" + (on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
 
-document.getElementById("toggle_sonar_btn").addEventListener("change", (event) => {
-    let sonar_on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("sonar_interval").value);
-    let pin = document.getElementById("sonar_pin").value;
+    });
+}
 
-    // Send request
-    send_api_request("sonar/" + (sonar_on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
-
-
-document.getElementById("toggle_gps_btn").addEventListener("change", (event) => {
-    let gps_on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("gps_interval").value);
-    let pin = document.getElementById("gps_pin").value;
-
-    // Send request
-    send_api_request("gps/" + (gps_on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
-
-
-document.getElementById("toggle_magneto_btn").addEventListener("change", (event) => {
-    let on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("magneto_interval").value);
-    let pin = document.getElementById("magneto_pin").value;
-
-    // Send request
-    send_api_request("magneto/" + (on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
-
-document.getElementById("toggle_gyro_btn").addEventListener("change", (event) => {
-    let on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("gyro_interval").value);
-    let pin = document.getElementById("gyro_pin").value;
-
-    // Send request
-    send_api_request("gyro/" + (on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
-
-
-document.getElementById("toggle_accel_btn").addEventListener("change", (event) => {
-    let on_off = event.target.checked;
-    let interval = parseInt(document.getElementById("accel_interval").value);
-    let pin = document.getElementById("accel_pin").value;
-
-    // Send request
-    send_api_request("accel/" + (on_off ? "on" : "off") + "?interval=" + interval + "&pin=" + pin);
-});
-
+addToggleEventListener("humidity");
+addToggleEventListener("sonar");
+addToggleEventListener("gps");
+addToggleEventListener("magneto");
+addToggleEventListener("gyro");
+addToggleEventListener("accel");
 
 
 document.getElementById("submit-thingsboard-access-code").onclick = () => {
